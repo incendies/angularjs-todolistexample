4cfe37a0d135b330a66e2a1705b3a3ed1ce6ae99
Yunus Emre Özdiyar <yunusemreozdiyar@gmail.com>
2017-05-26 00:42:10 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var app = angular.module("todolistapp", []);
+
+app.controller("todolistcontroller", function($scope) {
+
+		$scope.todos = [
+				{"name": "justdoit1", "result": false },
+				{"name": "justdoit2", "result": false },
+				{"name": "justdoit3", "result": true },
+				{"name": "justdoit4", "result": false }
+				
+		];
+
+		$scope.NewTodo = function () {
+			$scope.todos.push({
+				"name": $scope.todo,
+				"result": false
+				});
+
+			$scope.todo= "";
+			}
+
+			$scope.DeleteTodos= function () {
+				$scope.todos = $scope.todos.filter(function(item) {
+					return !item.result;	
+				});	
+			}
+
+});
